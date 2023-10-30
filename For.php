1427f33aad9dae180a8e1ce9dd6986f5a4dedf4a
Weirdo1_1 <samyjabarti@hotmail.com>
2023-10-30 23:56:41 +0300

    Finished For

---
+++ For.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$family = array("Nour", "Majed", "Anas");
+
+
+foreach ($family as $member){
+
+echo "$member <br>";
+
+
+}
+
+
+
+/*
+for( $i = 0; $i < count($family); $i++ ){
+echo "$family[$i] <br>";
+}*/
+
+
+
+?>
