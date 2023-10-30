c6fab24a1fa5ce15aed093e815c9808224a51ebf
Weirdo1_1 <samyjabarti@hotmail.com>
2023-10-30 23:10:52 +0300

    Finished IF and associates lesson

---
+++ if.php
@@ -0,0 +1,52 @@
+
+<?php
+$username = "Samei Jabarti";
+$email = "S32J@hotmail.com";
+
+if($username!== "Sami Jabarti" or $email !== "SJ@hotmail.com"){
+
+echo "Welcome Admin";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+<?php
+
+
+/*
+ $num1 = 20;
+ $num2 = 333;  
+
+if ($num1 > $num2) {
+    echo "num1 is greater than num2";
+
+if ($num1===20) {
+    echo "<br>";
+echo "num1 is equal to 20";
+}
+
+} else if ($num1 < $num2) {
+    echo "num1 is less than num2";
+} else {
+    echo "num1 is equal to num2";
+}
+*/
+
+
+?>
