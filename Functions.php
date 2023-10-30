ee43f0d0769768896f1ee4b780de01f43e282294
Weirdo1_1 <samyjabarti@hotmail.com>
2023-10-30 23:53:19 +0300

    Function finished

---
+++ Functions.php
@@ -0,0 +1,22 @@
+<?php
+
+function add($x, $y){
+    return $x + $y;
+    }
+    echo add(15, 10);
+    echo add (15, 20);
+    echo add (25, 30);
+
+
+
+function hello (){
+$user = "Anas";
+if($user=== "Nour"){
+echo "Hello" . $user;
+
+} else{
+echo "Bye" . $user;
+
+}
+}
+hello();
