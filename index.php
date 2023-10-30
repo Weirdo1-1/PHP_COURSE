714390fa12472e51ee001f3f7f9b063c16d6692f
Weirdo1_1 <samyjabarti@hotmail.com>
2023-10-30 21:39:51 +0300

    2nd Commit

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-phpinfo();
+
+$title = "Sami Jabarti";
+$num = 40;
+$float = 40.5;
+$bool = true;
+$bool2 = false;
+$nothing = null;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title . " - Home page"?></title>
+</head>
+<body>
+<?php echo "<h1>Welcome to $title</h1>"; 
+echo $num;
+echo "<br>";
+echo $float;
+?>
+</body>
+</html>
