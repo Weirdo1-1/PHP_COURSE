5a386c99280aae1713eeea86094c2d6689cbc205
Weirdo1_1 <samyjabarti@hotmail.com>
2023-10-30 21:56:41 +0300

    Finished strings lesson

---
+++ strings.php
@@ -0,0 +1,17 @@
+<?php
+$string = "Hello World";
+echo $string;
+echo "<br>";
+//echo strtolower($string);
+//echo strtoupper($string);
+//echo strlen($string);
+//echo $string[0];
+//$string [1] = "a";
+//echo $string;
+//echo str_replace ("World", "PHP", $string);
+//echo substr($string, 7, 3);
+
+
+
+
+?>
