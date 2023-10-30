ca0f0c41e1160dff91220d7115417554a3c393b7
Weirdo1_1 <samyjabarti@hotmail.com>
2023-10-30 22:06:27 +0300

    Finished math lesson

---
+++ math.php
@@ -0,0 +1,29 @@
+<?php
+//echo 20 / 10;
+//echo abs(100);
+//echo pow(2, 4);
+//echo sqrt(144);
+//echo round(2.5);
+//echo floor(2.9);
+//echo ceil(2.1);
+//echo 10%3;
+//$num = 10;
+//$num += 25;
+//echo $num;
+//echo max(2, 10, 12, 34, 45, 100);
+//echo "<br>";
+//echo min(2, 10, 12, 34, 45, 100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
